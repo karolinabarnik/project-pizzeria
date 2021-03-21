42bd1164f7a74757953935f0c40d3af800161977
Karolina Barnik <karolinabarnik@gmail.com>
2021-03-21 22:21:23 +0100

    Add class Product

--- dist/js/script.js
+++ dist/js/script.js
@@ -55,8 +55,11 @@
     const app = {
 
         initMenu: function () {
-            const testProduct = new Product();
-            console.log('testProduct:', testProduct);
+            const thisApp = this;
+            console.log('thisApp.data:', thisApp.data);
+            for (let productData in thisApp.data.products) {
+                new Product(productData, thisApp.data.products[productData]);
+            }
         },
 
         initData: function () {
@@ -79,10 +82,34 @@
     };
 
     class Product {
-        constructor() {
+        constructor(id, data) {
             const thisProduct = this;
 
+            thisProduct.id = id;
+            thisProduct.data = data;
+
+            thisProduct.renderInMenu();
+
             console.log('new Product:', thisProduct);
+
+        }
+        renderInMenu() {
+            const thisProduct = this;
+
+            /*generate HTML based on template*/
+            const generatedHTML = templates.menuProduct(thisProduct.data);
+
+            /*create element using utils.createElementFromHTML*/
+
+            thisProduct.element = utils.createDOMFromHTML(generatedHTML);
+
+            /*find menu container */
+
+            const menuContainer = document.querySelector(select.containerOf.menu);
+
+            /*add element to menu*/
+
+            menuContainer.appendChild(thisProduct.element);
         }
 
     }
